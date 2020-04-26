8d51d58e1162bfb616eccc7f4532575f6d9dca1a
Julien BORDELLIER <git@julienbordellier.com>
2020-04-26 10:42:29 +0200

    Use generators instead of List of List of List of ....

--- ascii/ascii.py
+++ ascii/ascii.py
@@ -1,54 +1,78 @@
 from PIL import Image
-from typing import List, Tuple
+from typing import List, Tuple, Iterator, Callable
+import itertools
+
 
 BRIGHTNESS_CHARACTERS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+CORRECT_SIZE_RATIO = 3
 
-def load_image(file_name: str) -> List[List[Tuple[int, int, int]]]:
+
+def load_image(file_name: str, size: Tuple[int, int]) -> Iterator[Tuple[int, int, int]]:
     im = Image.open(file_name)
-    max_x = im.size[0]
-    max_y = im.size[1]
     print("Successfully loaded image!")
     print("Format:", im.format)
     print("Size: {} x {}".format(im.size[0], im.size[1]))
+    yield from im.resize(size).getdata()
 
-    pixels = []
-    data = list(im.getdata())
-    for y in range(max_y):
-        pixels.append(data[y*max_x:(y*max_x + max_x)])
-    return pixels
 
-def colors_to_brightness(pixels: List[List[Tuple[int, int, int]]], brightness_function) -> List[List[int]]:
-    brightness_map = []
-    for y in range(len(pixels)):
-        brightness_map.append([int(brightness_function(pixels[y][x])) for x in range(len(pixels[y]))])
-    return brightness_map
+def colors_to_brightness(pixels: Iterator[Tuple[int, int, int]],
+        brightness_function: Callable[[Tuple[int, int, int]], int]) -> Iterator[int]:
+    for pixel in pixels:
+        yield brightness_function(pixel)
 
-def average_brightness(pixel: Tuple[int, int, int]) -> float:
+
+def average_brightness(pixel: Tuple[int, int, int]) -> int:
     r, g, b = pixel
-    return (r + g + b) / 3
+    return int((r + g + b) / 3)
 
-def brightness_to_ascii(pixels: List[List[int]], brightness_chars: str) -> List[List[str]]:
-    def brightness_to_char(brightness: int, chars: str) -> str:
-        i = int(brightness * len(chars) / 255)
-        return brightness_chars[i]
-    ascii_map = []
-    for y in range(len(pixels)):
-        ascii_map.append([brightness_to_char(pixels[y][x], brightness_chars) for x in range(len(pixels[y]))])
-    return ascii_map
 
-def print_ascii(pixels: List[List[str]]) -> None:
-    for y in range(len(pixels)):
-        line = []
-        for x in range(len(pixels[y])):
-            line.append("{}{}{}".format(pixels[y][x], pixels[y][x], pixels[y][x]))
-        print("".join(line))
+def lightness_brightness(pixel: Tuple[int, int, int]) -> int:
+    r, g, b = pixel
+    return int((max(r, g, b) + min(r, g, b)) / 2)
 
+
+def luminosity_brightness(pixel: Tuple[int, int, int]) -> int:
+    r, g, b = pixel
+    return int(0.21*r + 0.72*g + 0.07*b)
+
+
+def brightness_to_char(brightness: int, chars: str) -> str:
+    i = int(brightness * len(chars) / 255)
+    return chars[i]
+
+
+def brightness_to_ascii(pixels: Iterator[int], brightness_chars: str) -> Iterator[str]:
+    for pixel in pixels:
+        yield brightness_to_char(pixel, brightness_chars)
+
+
+def correct_size(pixels: Iterator[str], ratio: int) -> Iterator[str]:
+    for pixel in pixels:
+        yield from itertools.repeat(pixel, ratio)
+
+
+def print_ascii(pixels: Iterator[str], size_x: int) -> None:
+    i = 0
+    for pixel in pixels:
+        print(pixel, end="")
+        if i % size_x == 0:
+            print()
+        i += 1
+    print()
+
+
+# TODO: Add argparse for:
+#   * filename
+#   * resize (maybe use termcaps instead)
+#   * brightness function
+#   ...
 def main():
     file_name = "ascii-pineapple.jpg"
-    pixels = load_image(file_name)
-    brightness_pixels = colors_to_brightness(pixels, average_brightness)
+    pixels = load_image(file_name, (700, 467))
+    brightness_pixels = colors_to_brightness(pixels, lightness_brightness)
     ascii_pixels = brightness_to_ascii(brightness_pixels, BRIGHTNESS_CHARACTERS)
-    print_ascii(ascii_pixels)
+    corrected_size_ascii = correct_size(ascii_pixels, CORRECT_SIZE_RATIO)
+    print_ascii(corrected_size_ascii, 700*CORRECT_SIZE_RATIO)
 
 if __name__ == "__main__":
     main()
